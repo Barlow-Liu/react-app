c71ca2dd8ad83bdaf58eddbbc84c65d41645b58f
barlow <liuzhilin01@inspur.com>
2022-10-14 18:13:58 +0800

    learn

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 
+const user = {
+  name: 'Hedy Lamarr',
+  imageUrl: 'public/logo512.png',
+  imageSize: 90,
+};
+
 function App() {
   return (
     <div className="App">
@@ -18,8 +24,27 @@ function App() {
           Learn React
         </a>
       </header>
+      <h1>Welcome to my app</h1>
+      <h1>{user.name}</h1>
+      <MyButton />
+      <h1>{user.name}</h1>
+      <img
+        className="avatar"
+        src={user.imageUrl}
+        alt={'Photo of ' + user.name}
+        style={{
+          width: user.imageSize,
+          height: user.imageSize
+        }}
+      />
     </div>
+
   );
 }
+
+function MyButton() {
+  return (<button>Click me </button>);
+}
+
 
 export default App;
